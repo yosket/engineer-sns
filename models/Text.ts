b09be14d9d864231831a6a94a521772630e737b9
Yosuke Tanaka <yosket87@gmail.com>
2021-07-24 16:19:00 +0900

    Show recent texts

---
+++ models/Text.ts
@@ -0,0 +1,7 @@
+export type Text = {
+  id: string
+  text: string
+  _created_at: string
+  _updated_at: string
+  _user_id: string
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,23 @@
 import { NextPage } from 'next'
+import { useAllText } from '../hooks/useText'
+import { Text } from '../models/Text'
 
 const HomePage: NextPage = () => {
-  return <h1>Engineer SNS</h1>
+  const { data: allText } = useAllText()
+
+  return (
+    <div className="space-y-4 p-4">
+      {allText?.map((t: Text) => (
+        <article className="border border-gray-200 p-4 rounded" key={t.id}>
+          <p>{t.id}</p>
+          <p>{t.text}</p>
+          <p>{t._created_at}</p>
+          <p>{t._updated_at}</p>
+          <p>{t._user_id}</p>
+        </article>
+      ))}
+    </div>
+  )
 }
 
 export default HomePage
