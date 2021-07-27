ee65f73bfcb0cff6e43af8d5fee7d35381e3b131
Yosuke Tanaka <yosket87@gmail.com>
2021-07-27 12:03:40 +0900

    Update AppContainer.tsx

--- components/AppContainer.tsx
+++ components/AppContainer.tsx
@@ -4,9 +4,9 @@ import AppHeader from './AppHeader'
 
 const AppContainer: FC = ({ children }) => {
   return (
-    <div className="bg-white relative min-h-screen">
+    <div className="bg-white relative min-h-screen flex flex-col">
       <AppHeader className="sticky top-0 w-full z-10" />
-      <main className="bg-white">{children}</main>
+      <main className="bg-white flex-1">{children}</main>
       <AppFooter />
     </div>
   )
